6ed1afe8100d1e1807cf5c31c0cca37ce5542723
nunocarvalh <83798150+nunocarvalh@users.noreply.github.com>
2021-05-07 22:22:54 -0300

    Add files via upload

---
+++ ex01raio.py
@@ -0,0 +1,11 @@
+raio = float(input(""))
+pi = 3.141592
+c_circ = 2*pi*raio
+a_circ = pi*raio**2
+a_esfe = 4*pi*raio**2
+v_esfe = 4*pi*raio**3/3
+print(f'{c_circ:.6f}')
+print(f'{a_circ:.6f}')
+print(f'{a_esfe:.6f}')
+print(f'{v_esfe:.6f}')
+
